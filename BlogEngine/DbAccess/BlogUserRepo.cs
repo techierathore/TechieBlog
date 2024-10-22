9ec102972376ca58c50a92dfdf5d438e381fbd10
S Ravi Kumar <techierathore@gmail.com>
2024-10-22 22:35:20 +0530

    Just for the streak

--- BlogEngine/DbAccess/BlogUserRepo.cs
+++ BlogEngine/DbAccess/BlogUserRepo.cs
@@ -1,32 +1,34 @@
-﻿namespace BlogEngine.DbAccess;
+﻿using BlogModels.ViewModel;
 
-public class BlogUserRepo : GenericRepository<BlogUser>, IBlogUserRepo
+namespace BlogEngine.DbAccess;
+
+public class BlogUserRepo : GenericRepository<AppUser>, IBlogUserRepo
 {
     public BlogUserRepo(string connectionString) : base(connectionString) { }
 
-    public override IEnumerable<BlogUser> GetAll()
+    public override IEnumerable<AppUser> GetAll()
     {
         using var vConn = GetOpenConnection();
-        return vConn.Query<BlogUser>("BlogUsersSelectAll", commandType: CommandType.StoredProcedure);
+        return vConn.Query<AppUser>("BlogUsersSelectAll", commandType: CommandType.StoredProcedure);
     }
-    public override IEnumerable<BlogUser> GetAllById(long aSingleId)
+    public override IEnumerable<AppUser> GetAllById(long aSingleId)
     {
         throw new System.NotImplementedException();
     }
-    public override BlogUser GetIntSingle(int aSingleId)
+    public override AppUser GetIntSingle(int aSingleId)
     {
         throw new System.NotImplementedException();
     }
 
-    public override BlogUser GetSingle(long aSingleId)
+    public override AppUser GetSingle(long aSingleId)
     {
         using var vConn = GetOpenConnection();
         var vParams = new DynamicParameters();
         vParams.Add("@UserId", aSingleId);
-        return vConn.QueryFirstOrDefault<BlogUser>("AppUserSelect", vParams, commandType: CommandType.StoredProcedure);
+        return vConn.QueryFirstOrDefault<AppUser>("AppUserSelect", vParams, commandType: CommandType.StoredProcedure);
     }
 
-    public override void Insert(BlogUser aAppUser)
+    public override void Insert(AppUser aAppUser)
     {
         using var vConn = GetOpenConnection();
         var vParams = new DynamicParameters();
@@ -35,12 +37,12 @@ public class BlogUserRepo : GenericRepository<BlogUser>, IBlogUserRepo
         vParams.Add("@EmailID", aAppUser.EmailId);
         vParams.Add("@loginPassword", aAppUser.LoginPass);
         vParams.Add("@Role", aAppUser.UserRole);
-        vParams.Add("@CreatedTime", aAppUser.CreatedTime);
-        vParams.Add("@UpdatedTime", aAppUser.UpdatedTime);
+        //vParams.Add("@CreatedTime", aAppUser.CreatedTime);
+        //vParams.Add("@UpdatedTime", aAppUser.UpdatedTime);
         int iResult = vConn.Execute("BlogUsersInsert", vParams, commandType: CommandType.StoredProcedure);
     }
 
-    public override long InsertToGetId(BlogUser aAppUser)
+    public override long InsertToGetId(AppUser aAppUser)
     {
         long lLastInsertedId = 0;
         using var vConn = GetOpenConnection();
@@ -50,15 +52,15 @@ public class BlogUserRepo : GenericRepository<BlogUser>, IBlogUserRepo
         vParams.Add("@EmailID", aAppUser.EmailId);
         vParams.Add("@loginPassword", aAppUser.LoginPass);
         vParams.Add("@Role", aAppUser.UserRole);
-        vParams.Add("@CreatedTime", aAppUser.CreatedTime);
-        vParams.Add("@UpdatedTime", aAppUser.UpdatedTime);
+        //vParams.Add("@CreatedTime", aAppUser.CreatedTime);
+        //vParams.Add("@UpdatedTime", aAppUser.UpdatedTime);
         vParams.Add("@pInsertedId", lLastInsertedId, direction: ParameterDirection.Output);
         int iResult = vConn.Execute("AppUserInsert4Id", vParams, commandType: CommandType.StoredProcedure);
         lLastInsertedId = vParams.Get<long>("@pInsertedId");
         return lLastInsertedId;
     }
 
-    public override void Update(BlogUser aBlogUser)
+    public override void Update(AppUser aBlogUser)
     {
         using var vConn = GetOpenConnection();
         var vParams = new DynamicParameters();
@@ -68,37 +70,37 @@ public class BlogUserRepo : GenericRepository<BlogUser>, IBlogUserRepo
         vParams.Add("@EmailID", aBlogUser.EmailId);
         vParams.Add("@loginPassword", aBlogUser.LoginPass);
         vParams.Add("@Role", aBlogUser.UserRole);
-        vParams.Add("@CreatedTime", aBlogUser.CreatedTime);
-        vParams.Add("@UpdatedTime", aBlogUser.UpdatedTime);
-        vParams.Add("@LastLogin", aBlogUser.LastLogin);
+        //vParams.Add("@CreatedTime", aBlogUser.CreatedTime);
+        //vParams.Add("@UpdatedTime", aBlogUser.UpdatedTime);
+        //vParams.Add("@LastLogin", aBlogUser.LastLogin);
         vConn.Execute("BlogUsersUpdate", vParams, commandType: CommandType.StoredProcedure);
     }
 
-    public BlogUser GetLoginUser(string aLoginEmail, string aPassword)
+    public AppUser GetLoginUser(string aLoginEmail, string aPassword)
     {
         using var vConn = GetOpenConnection();
         var vParams = new DynamicParameters();
         vParams.Add("@LoginMail", aLoginEmail);
         vParams.Add("@LoginPassword", aPassword);
-        var vReturnUser = vConn.Query<BlogUser>("GetLoginUser", vParams, commandType: CommandType.StoredProcedure).SingleOrDefault();
+        var vReturnUser = vConn.Query<AppUser>("GetLoginUser", vParams, commandType: CommandType.StoredProcedure).SingleOrDefault();
         return vReturnUser;
     }
 
-    public BlogUser GetUserByEmail(string aLoginEmail)
+    public AppUser GetUserByEmail(string aLoginEmail)
     {
         using var vConn = GetOpenConnection();
-        var vReturnUser = vConn.Query<BlogUser>("GetUserByEmail", new { LoginMail = aLoginEmail }, commandType: CommandType.StoredProcedure).SingleOrDefault();
+        var vReturnUser = vConn.Query<AppUser>("GetUserByEmail", new { LoginMail = aLoginEmail }, commandType: CommandType.StoredProcedure).SingleOrDefault();
         return vReturnUser;
     }
 
-    public BlogUser GetUserByMobile(string aMobileNo)
+    public AppUser GetUserByMobile(string aMobileNo)
     {
         using var vConn = GetOpenConnection();
-        var vReturnUser = vConn.Query<BlogUser>("GetUserByMobile", new { UserMobileNo = aMobileNo }, commandType: CommandType.StoredProcedure).SingleOrDefault();
+        var vReturnUser = vConn.Query<AppUser>("GetUserByMobile", new { UserMobileNo = aMobileNo }, commandType: CommandType.StoredProcedure).SingleOrDefault();
         return vReturnUser;
     }
 
-    public override IEnumerable<BlogUser> GetPagedData(int PageSize, int OffSet)
+    public override IEnumerable<AppUser> GetPagedData(int PageSize, int OffSet)
     {
         throw new NotImplementedException();
     }
