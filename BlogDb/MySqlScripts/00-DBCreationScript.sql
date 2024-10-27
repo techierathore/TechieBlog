ac1a7d04eb710411d1cba572e70c743659797b76
S Ravi Kumar <techierathore@gmail.com>
2024-10-27 23:00:04 +0530

    Added Back the Role into User Script as it was getting too complicated.

--- BlogDb/MySqlScripts/00-DBCreationScript.sql
+++ BlogDb/MySqlScripts/00-DBCreationScript.sql
@@ -1,18 +1,12 @@
-CREATE TABLE UserRole (
-    RoleId INT AUTO_INCREMENT PRIMARY KEY,
-    RoleName VARCHAR(255) NOT NULL,
-    RoleDesc VARCHAR(555)
-);
-
 CREATE TABLE BlogUser (
     UserId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(255) NOT NULL,
-    LastName VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
     EmailId VARCHAR(255) NOT NULL UNIQUE,
     LoginPass VARCHAR(255) NOT NULL,
     CreatedOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UpdatedOn DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    UserRoleId INT,
+    UserRole VARCHAR(51) NOT NULL,
     IsConfirmed bit(1) DEFAULT b'0',
     ProfileImagePath VARCHAR(255),
     ProfileDescription TEXT,
@@ -21,5 +15,4 @@ CREATE TABLE BlogUser (
     GitHubUrl VARCHAR(255),
     PodDescription VARCHAR(1050),
     SpeakDescription VARCHAR(1050)
-    CONSTRAINT FK_UserRole FOREIGN KEY (UserRoleId) REFERENCES UserRole(RoleId)
 );
