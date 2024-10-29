e808c15dd0fc796f5cd99e437d34c0e91cd0ec6e
S Ravi Kumar <techierathore@gmail.com>
2024-10-29 23:17:43 +0530

    Added Blog and Tag Services

---
+++ BlogEngine/Services/BlogSvc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogEngine.Services;
+
+public class BlogSvc
+{
+    private readonly IBlogPostRepo PostRepo;
+
+    public BlogSvc(IBlogPostRepo aPostRepo)
+    {
+        PostRepo = aPostRepo;
+    }
+
+    public IEnumerable<BlogPost> GetAllPosts(long aUserId, bool aIsAdmin)
+    {
+        IEnumerable<BlogPost> vReturnVal;
+        if (aIsAdmin)
+        {
+            vReturnVal = PostRepo.GetAll();
+        }
+        else vReturnVal = PostRepo.GetAllById(aUserId);
+        return vReturnVal;
+    }
+
+    public BlogPost GetSinglePost(long aSingleId)
+    {
+        var vReturnVal = PostRepo.GetSingle(aSingleId);
+        return vReturnVal;
+    }
+    public void SavePost(BlogPost aObject)
+    {
+        PostRepo.Insert(aObject);
+    }
+    public void UpdatePost( BlogPost aObject)
+    {
+        PostRepo.Update(aObject);
+    }
+
+}
---
+++ BlogEngine/Services/TagSvc.cs
@@ -0,0 +1,29 @@
+﻿
+namespace BlogEngine.Services;
+
+public class TagSvc
+{
+    private readonly IBlogTagRepo TagRepo;
+
+    public TagSvc(IBlogTagRepo aTagRepo)
+    {
+        TagRepo = aTagRepo;
+    }
+    public IEnumerable<BlogTag> GetAllTags()
+    {
+        IEnumerable<BlogTag> vReturnVal = TagRepo.GetAll(); ;
+        return vReturnVal;
+    }
+    public BlogTag GetSingleTag(long aSingleId)
+    {
+        var vReturnVal = TagRepo.GetSingle(aSingleId);
+        return vReturnVal;
+    }
+
+    public void AddTag(BlogTag aObject) {
+        TagRepo.Insert(aObject);
+    }
+
+    public void UpdateTag(BlogTag aObject) { TagRepo.Update(aObject); }
+
+}
