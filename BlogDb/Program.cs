411b441c248262a3ce21e96d659a8965b2c8ea9c
S Ravi Kumar <techierathore@gmail.com>
2024-11-23 23:55:32 +0530

    Added DB Creation Code.

--- BlogDb/Program.cs
+++ BlogDb/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// TODO Add the Code to setup the datbase using code. 
+using DbUp;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please provide the MySQL connection string as a parameter.");
+            return -1;
+        }
+
+        var connectionString = args[0];
+
+        var upgrader =
+            DeployChanges.To
+                .MySqlDatabase(connectionString)
+                .WithScriptsFromFileSystem("MySqlScripts")
+                .LogToConsole()
+                .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+            return -1;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Success!");
+        Console.ResetColor();
+        return 0;
+    }
+}
