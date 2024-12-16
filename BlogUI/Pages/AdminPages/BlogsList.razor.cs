b9d068e0b408e4cfa5ed618a9388b547b0593b62
S Ravi Kumar <techierathore@gmail.com>
2024-12-16 23:10:00 +0530

    Started working on pages

--- BlogUI/Pages/AdminPages/BlogsList.razor.cs
+++ BlogUI/Pages/AdminPages/BlogsList.razor.cs
@@ -1,6 +1,27 @@
-﻿namespace BlogUI.Pages.BlogPages;
+﻿using BlogModels.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+using System.Security.Principal;
+using BlogModels;
 
-partial class BlogsList
+namespace BlogUI.Pages.BlogPages;
+
+partial class BlogsList : ComponentBase
 {
-
+    [Inject]
+    public IManageService<BlogPost> DataService { get; set; }
+    public List<BlogPost> ObjectList { get; set; }
+    public BlogPost SelObject { get; set; }
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthStateTask { get; set; }
+    ClaimsPrincipal LoggedInUser;
+    protected override async Task OnInitializedAsync()
+    {
+        LoggedInUser = (await AuthStateTask).User;
+        string vEmpId = LoggedInUser.Claims.FirstOrDefault(
+            c => c.Type == ClaimTypes.PrimarySid)?.Value;
+        long lEmployeeId = Convert.ToInt64(vEmpId);
+       // ObjectList = await DataService.GetAllSubsAsync(ClientConstants.UserAccSvcUrl, lEmployeeId);
+    }
 }
