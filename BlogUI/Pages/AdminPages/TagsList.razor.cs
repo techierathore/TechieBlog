2c333fe4e8a96d6cc9658b46bbf89e4b1c6e202e
S Ravi Kumar <techierathore@gmail.com>
2024-12-18 22:57:14 +0530

    Desugned Tags List Page

--- BlogUI/Pages/AdminPages/TagsList.razor.cs
+++ BlogUI/Pages/AdminPages/TagsList.razor.cs
@@ -1,6 +1,26 @@
-﻿namespace BlogUI.Pages.AdminPages;
+﻿using BlogModels.Interfaces;
+using BlogModels;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
 
-partial class TagsList
+namespace BlogUI.Pages.AdminPages;
+
+partial class TagsList : ComponentBase
 {
-
+    [Inject]
+    public IManageService<BlogTag> DataService { get; set; }
+    public List<BlogTag> ObjectList { get; set; }
+    public BlogTag SelObject { get; set; }
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthStateTask { get; set; }
+    ClaimsPrincipal LoggedInUser;
+    protected override async Task OnInitializedAsync()
+    {
+        LoggedInUser = (await AuthStateTask).User;
+        string vEmpId = LoggedInUser.Claims.FirstOrDefault(
+            c => c.Type == ClaimTypes.PrimarySid)?.Value;
+        long lEmployeeId = Convert.ToInt64(vEmpId);
+        // ObjectList = await DataService.GetAllSubsAsync(ClientConstants.UserAccSvcUrl, lEmployeeId);
+    }
 }
