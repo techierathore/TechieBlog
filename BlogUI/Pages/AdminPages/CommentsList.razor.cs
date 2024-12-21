5677155d17d50f89cff0bb936546354e37d58828
S Ravi Kumar <techierathore@gmail.com>
2024-12-21 22:53:29 +0530

    Added Comments List

---
+++ BlogUI/Pages/AdminPages/CommentsList.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlogModels.Interfaces;
+using BlogModels;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+
+namespace BlogUI.Pages.AdminPages;
+
+partial class CommentsList : ComponentBase
+{
+    [Inject]
+    public IManageService<BlogPost> DataService { get; set; }
+    public List<BlogPost> ObjectList { get; set; }
+    public BlogPost SelObject { get; set; }
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthStateTask { get; set; }
+    ClaimsPrincipal LoggedInUser;
+    protected override async Task OnInitializedAsync()
+    {
+        LoggedInUser = (await AuthStateTask).User;
+        string vEmpId = LoggedInUser.Claims.FirstOrDefault(
+            c => c.Type == ClaimTypes.PrimarySid)?.Value;
+        long lEmployeeId = Convert.ToInt64(vEmpId);
+        // ObjectList = await DataService.GetAllSubsAsync(ClientConstants.UserAccSvcUrl, lEmployeeId);
+    }
+}
