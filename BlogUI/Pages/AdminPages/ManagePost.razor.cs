cecc3e438e454ce421be913c65c75239aed2a1a6
S Ravi Kumar <techierathore@gmail.com>
2024-12-25 21:56:52 +0530

    Manage Post In Progress

--- BlogUI/Pages/AdminPages/ManagePost.razor.cs
+++ BlogUI/Pages/AdminPages/ManagePost.razor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BlogModels.Interfaces;
+using BlogModels;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +9,19 @@ using System.Threading.Tasks;
 
 namespace BlogUI.Pages.AdminPages;
 
-partial class ManagePost
+partial class ManagePost : ComponentBase
 {
+    [Parameter]
+    public long PageId { get; set; }
+    [Inject]
+    NavigationManager AppNavManager { get; set; }
+    [Inject]
+    public IManageService<BlogPost> DataService { get; set; }
+    public string PageHeader { get; set; }
+    public BlogPost PageObj { get; set; }
+
+    protected void SaveData()
+    {
+
+    }
 }
