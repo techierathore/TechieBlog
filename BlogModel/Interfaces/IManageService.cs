e738765400f4e6147c8b1a52bf1d333ba096b892
S Ravi Kumar <techierathore@gmail.com>
2024-12-26 22:00:23 +0530

    In Progress Changes

--- BlogModel/Interfaces/IManageService.cs
+++ BlogModel/Interfaces/IManageService.cs
@@ -5,14 +5,24 @@
 /// <typeparam name="TEntity"></typeparam>
 public interface IManageService<TEntity>
 {
-    Task<List<TEntity>> GetAllAsync(string aRequestUri);
-    Task<List<TEntity>> GetAllSubsAsync(string aRequestUri, long aId);
+    Task<List<TEntity>> GetAllListAsync(string aRequestUri);
+    Task<List<TEntity>> GetSubsListAsync(string aRequestUri, long aId);
     Task<List<TEntity>> GetAllByStringAsync(string aRequestUri, string aValue);
     Task<TEntity> GetSingleAsync(string aRequestUri, long aId);
     Task<TEntity> GetIntSingleAsync(string aRequestUri, int aId);
     Task<TEntity> SaveAsync(string aRequestUri, TEntity aObj);
     Task<TEntity> UpdateAsync(string aRequestUri, TEntity aObj);
     Task<List<TEntity>> GetReportAsync(string aRequestUri, ReportInput aObj);
-    Task<bool> UploadFile(string aRequestUri, TEntity aObj, Stream aFiles, string aFileName);
-    Task<byte[]> DownloadFile(string aRequestUri, long aId);
+
+    List<TEntity> GetAllList(string aRequestUri);
+    List<TEntity> GetSubsList(string aRequestUri, long aId);
+    List<TEntity> GetSubsListByString(string aRequestUri, string aValue);
+    TEntity GetSingle(string aRequestUri, long aId);
+    TEntity GetIntSingle(string aRequestUri, int aId);
+    TEntity Save(string aRequestUri, TEntity aObj);
+    TEntity Update(string aRequestUri, TEntity aObj);
+    List<TEntity> GetReport(string aRequestUri, ReportInput aObj);
+    bool UploadFile(string aRequestUri, TEntity aObj, Stream aFiles, string aFileName);
+    byte[] DownloadFile(string aRequestUri, long aId);
+
 }
